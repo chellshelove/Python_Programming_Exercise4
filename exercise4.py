91545a7676008a2b5ceb48636e6ffff959f6ee8c
chellshelove <chellshelove733@gmail.com>
2024-10-02 11:13:46 +0900

    done exercise 4

---
+++ exercise4.py
@@ -0,0 +1,8 @@
+# Get input string from the user
+input_string = input("Enter a string: ")
+
+# Reverse the string using slicing
+reversed_string = input_string[::-1]
+
+# Print the reversed string
+print("Reversed string:", reversed_string)
